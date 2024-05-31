7b4c16d99ec46015227a60682458ede60b3ce57e
Iván Agustín Bravo <98109767+bravoivanagustin@users.noreply.github.com>
2024-05-31 13:07:49 -0300

    nashe

--- Taller5/ABB.java
+++ Taller5/ABB.java
@@ -6,19 +6,20 @@ import java.util.*;
 // elem1.compareTo(elem2) devuelve un entero. Si es mayor a 0, entonces elem1 > elem2
 public class ABB<T extends Comparable<T>> implements Conjunto<T> {
     
-    private Nodo padre;
+    private Nodo raiz;
     private int longitud;
 
     private class Nodo {
+        Nodo papi;
         Nodo izq;
         Nodo der;
         T valor;
 
-        Nodo(T elem){this.valor = elem; this.izq = null; this.der = null;}
+        Nodo(T elem){this.valor = elem;}
     }
 
     public ABB() {
-        this.padre = null;
+        this.raiz = null;
         this.longitud = 0;
     }
 
@@ -27,45 +28,81 @@ public class ABB<T extends Comparable<T>> implements Conjunto<T> {
     }
 
     public T minimo(){
-        Nodo nuevo = this.padre;
-        if (nuevo == null) {
-            return null;
-        }
-        while (nuevo.izq != null) {
-            nuevo = nuevo.izq;
-        }
-        return nuevo.valor;
+        Nodo actual = this.raiz;
+        while (actual.izq != null) {actual = actual.izq;}
+        return actual.valor;
     }
 
     public T maximo(){
-        Nodo nuevo = this.padre;
-        if (nuevo == null) {
-            return null;
-        }
-        while (nuevo.der != null) {
-            nuevo = nuevo.der;
-        }
-        return nuevo.valor;
+        Nodo actual = this.raiz;
+        while (actual.der != null) {actual = actual.der;}
+        return actual.valor;
     }
 
     public void insertar(T elem){
-        Nodo actual = this.padre;
-        Nodo nuevo = new Nodo(elem);
+
+        if (!this.pertenece(elem)){
+
+        Nodo actual = this.raiz;
+        Nodo insertar = new Nodo(elem);
         
-        while (actual != null) {
-            if (nuevo.valor.compareTo(actual.valor) > 0) {actual = actual.der;}
+        if (this.raiz == null) {this.raiz = insertar;} 
+
+        else{
+        while ((insertar.valor.compareTo(actual.valor) > 0 && actual.der != null) || (insertar.valor.compareTo(actual.valor) < 0 && actual.izq != null) ){
+            if (insertar.valor.compareTo(actual.valor) > 0) {actual = actual.der;}
             else {actual = actual.izq;}
         }
-
-        
+        if (insertar.valor.compareTo(actual.valor) > 0) {actual.der = insertar; insertar.papi = actual;}
+        else {actual.izq = insertar;  insertar.papi = actual;}
+        }
+        longitud ++;
+        }
     }
 
     public boolean pertenece(T elem){
-        throw new UnsupportedOperationException("No implementada aun");
+        Nodo actual = this.raiz;
+        while (actual != null && actual.valor != elem) {
+            if (elem.compareTo(actual.valor) > 0) {actual = actual.der;}
+            else {actual = actual.izq;}
+        }
+        return actual != null;
+    }
+
+    private Nodo sucesor(Nodo nashe){
+        Nodo suc = nashe.der;
+        while (suc.izq != null) {suc = suc.izq;}
+        return suc;
     }
 
     public void eliminar(T elem){
-        throw new UnsupportedOperationException("No implementada aun");
+        if (this.pertenece(elem)){
+        
+        Nodo actual = this.raiz;
+
+        while (actual.valor != elem){
+            if (elem.compareTo(actual.valor) > 0) {actual = actual.der;}
+            else {actual = actual.izq;}
+            }
+        
+        if (actual.der == actual.izq) {actual = null;}
+
+        else if (actual.der == null || actual.izq == null) {
+            if (actual.der != null){
+                actual.der.papi = actual.papi;
+            }
+            else{
+                actual.izq.papi = actual.papi;
+            }
+        }
+
+        else if (actual.der != null && actual.izq != null) {
+
+        }
+
+        longitud --;
+
+        }
     }
 
     public String toString(){
